6d64c9a3906b9b112c26dd78010abb1c2552cf79
Hari <hariharan@zed.digital>
2023-05-06 10:42:09 +0530

    new bug fix

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
     NotificationScreen(),
         Container(),
         Container(), 
-        MessageScreen(),
+        MessageScreen(), 
         ProfileScreen(), 
 
   ];
--- lib/view/messages.dart
+++ lib/view/messages.dart
@@ -17,7 +17,7 @@ class _MessageScreenState extends State<MessageScreen> {
         height: Responsive.screenHeight,
         child: ListView.separated(
           itemCount: 2, 
-          separatorBuilder: (context, index) => SizedBox(height: 10,),
+          separatorBuilder: (context, index) => SizedBox(height: 10,), 
           itemBuilder: (context, index) => Padding(
             padding: EdgeInsets.all(10), 
             child: Material(
